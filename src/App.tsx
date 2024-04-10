5fa528de745214aae3d73125fd0817b3a455a257
dinar1122 <gert.olh@mail.ru>
2024-04-10 08:23:41 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react"
+import "./App.css"
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button>aaa</Button>
+    </div>
+  )
+}
+
+export default App
